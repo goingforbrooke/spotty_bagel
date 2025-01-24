28350ea403bf430ffdbc10271a2298a10edd6d4e
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2025-01-23 16:34:10 -0800

    Add deps inline in `main.py`

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "requests",
+# ]
+# ///
 """Spotty Bagel is a command line application for opening the current [BAGeL Radio]() song in your [Spotify]() application."""
 from logging import basicConfig, DEBUG, error, INFO, info, warning
 
